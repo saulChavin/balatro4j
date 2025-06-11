b42a870fe5d04951eb1696a9a1896e2c829c2f8b
saulChavin <sagus_97@hotmail.com>
2025-06-11 09:57:37 -0600

    feat: add PackKind, PackType, and Spectral enums

---
+++ docs/js/immolate2/src/enums/packs/Spectral.js
@@ -0,0 +1,31 @@
+
+
+const Spectral = {
+    // Mystical Entities
+    FAMILIAR: "Familiar",
+    GRIM: "Grim",
+    WRAITH: "Wraith",
+    MEDIUM: "Medium",
+    CRYPTID: "Cryptid",
+
+    // Spiritual Objects
+    TALISMAN: "Talisman",
+    SIGIL: "Sigil",
+    OUIJA: "Ouija",
+    ANKH: "Ankh",
+
+    // Paranormal Phenomena
+    INCANTATION: "Incantation",
+    AURA: "Aura",
+    ECTOPLASM: "Ectoplasm",
+    IMMOLATE: "Immolate",
+    DEJA_VU: "Deja Vu",
+    HEX: "Hex",
+    TRANCE: "Trance",
+
+    // System
+    RETRY: "RETRY",
+    RETRY2: "RETRY"
+}
+
+export default Object.freeze(Spectral);
